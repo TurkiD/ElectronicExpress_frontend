72be6413e0a6fcb6b0e25ac0f6ed84d87331ff52
Turki Dhawi <T.Dhawi@outlook.com>
2024-05-27 21:58:00 +0300

    fixed error when to try to register

--- src/toolkit/slices/userSlice.tsx
+++ src/toolkit/slices/userSlice.tsx
@@ -1,5 +1,5 @@
 import api from "@/api"
-import { LoginFormData, User, UserState } from "@/types/User"
+import { LoginFormData, RegisterFormData, User, UserState } from "@/types/User"
 import { getToken } from "@/utils/localStorage"
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 
@@ -47,7 +47,7 @@ export const fetchUsers = createAsyncThunk(
   }
 )
 
-export const registerUser = createAsyncThunk("users/registerUser", async (newUser: User) => {
+export const registerUser = createAsyncThunk("users/registerUser", async (newUser: RegisterFormData) => {
   const response = await api.post(`/register`, newUser)
   return response.data
 })
