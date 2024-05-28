a20f70a95c6bfd469043199f9b871dca8529bea0
Turki Dhawi <T.Dhawi@outlook.com>
2024-05-28 09:28:41 +0300

    added the backend url

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,7 @@ let baseURL = 'https://sda-online-2-csharp-backend-teamwork-7dbl.onrender.com/ap
 
 if (!isDevelopment) {
   // Update this later when you have a working backend server
-  baseURL = 'http://localhost:5125/api/v1'
+  baseURL = 'https://electronicexpress.netlify.app/api/v1'
 }
 
 const api = axios.create({
