e572a2e189ded5e1e65398e0b09283050e35bc08
Turki Dhawi <T.Dhawi@outlook.com>
2024-05-28 09:55:39 +0300

    updated the backend url

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,7 @@ let baseURL = 'https://sda-online-2-csharp-backend-teamwork-7dbl.onrender.com/ap
 
 if (!isDevelopment) {
   // Update this later when you have a working backend server
-  baseURL = 'https://sda-online-2-csharp-backend-teamwork-7dbl.onrender.com/'
+  baseURL = 'http://localhost:5125/api/'
 }
 
 const api = axios.create({
